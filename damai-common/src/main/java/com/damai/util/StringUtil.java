45e7039095138ea25a409d10b2b063c10cee2e50
ShiYue1026 <shiyue20011026@126.com>
2024-11-28 10:46:02 +0800

    工具

---
+++ damai-common/src/main/java/com/damai/util/StringUtil.java
@@ -0,0 +1,47 @@
+package com.damai.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Slf4j
+public class StringUtil {
+
+    public static boolean isNotEmpty(String str) {
+        return (str != null && !str.isEmpty() && !str.trim().isEmpty() && !"null".equalsIgnoreCase(str.trim())
+                && !"undefined".equalsIgnoreCase(str.trim()) && !"NULL".equalsIgnoreCase(str.trim()));
+    }
+
+    public static String inputStreamConvertString(InputStream is) {
+        ByteArrayOutputStream baos = null;
+        String result = null;
+
+        try{
+            if(is != null){
+                baos = new ByteArrayOutputStream();
+                int i;
+                while ((i = is.read()) != -1) {
+                    baos.write(i);
+                }
+                result = baos.toString();
+            }
+        }
+        catch(Exception e){
+            throw new RuntimeException("流转换为字符串失败！");
+        }
+        finally {
+            if(baos != null){
+                try {
+                    baos.close();
+                } catch (IOException e) {
+                    log.error("关闭流失败！");
+                }
+            }
+        }
+        return result;
+    }
+}
